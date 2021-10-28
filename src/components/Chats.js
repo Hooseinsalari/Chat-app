851922b631c130d5914a5085b2ffe6c42ae256bc
Hooseinsalari <hossein.salari002@gmail.com>
2021-10-28 15:27:41 +0330

    create logout and use chat engine

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
+import { ChatEngine } from "react-chat-engine";
 
 // Navbar
 import Navbar from './Navbar';
 
+// styles
+import styles from "./Chats.module.css";
+
 const Chats = () => {
+
+    const history = useHistory();
+
+    const logoutHandler = async () => {
+        await auth.signOut();
+        history.push("/")
+    }
     return (
-        <div>
-            <Navbar />
+        <div className={styles.container}>
+            <Navbar logOutHandler={logoutHandler} />
+            <ChatEngine 
+                height="90vh"
+                projectID="9aed24fb-01d8-460c-945b-1922499a9f9c"
+                userName="."
+                userSecret="."
+            />
         </div>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import React from 'react';
 // styles
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = ({logOutHandler}) => {
     return (
         <div className={styles.container}>
             <div className={styles.title}>
                 <h2>Hosagram</h2>
             </div>
             <div className={styles.logout}>
-                <button>Logout</button>
+                <button onClick={logOutHandler}>Logout</button>
             </div>
         </div>
     );
